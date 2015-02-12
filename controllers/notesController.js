554a9341962944aedd3e347caf8839f48d1b0e0d
vgaltes <vicenc.garcia-altes@valtech.co.uk>
2015-02-12 12:37:00 +0000

    First commit

---
+++ controllers/notesController.js
@@ -0,0 +1,50 @@
+﻿// notesController.js
+(function (notesController) {
+
+  var data = require("../data");
+  var auth = require("../auth");
+
+  notesController.init  = function (app) {
+
+    app.get("/api/notes/:categoryName", 
+      auth.ensureApiAuthenticated,
+      function (req, res) {
+
+        var categoryName = req.params.categoryName;
+
+        data.getNotes(categoryName, function (err, notes) {
+          if (err) {
+            res.send(400, err);
+          } else {
+            res.set("Content-Type", "application/json");
+            res.send(notes.notes);
+          }
+        });
+      }); 
+
+    app.post("/api/notes/:categoryName", 
+      auth.ensureApiAuthenticated,
+      function (req, res) {
+
+        var categoryName = req.params.categoryName;
+
+        var noteToInsert = {
+          note: req.body.note,
+          color: req.body.color,
+          author: "Shawn Wildermuth"
+        };
+
+        data.addNote(categoryName, noteToInsert, function (err) {
+          if (err) { 
+            res.send(400, "Failed to add note to data store");
+          } else {
+            res.set("Content-Type", "application/json");
+            res.send(201, noteToInsert);
+          }
+        });
+
+      });
+
+    };
+
+})(module.exports);
